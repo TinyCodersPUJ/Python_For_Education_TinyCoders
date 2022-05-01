23de429c23fcd4b8d21330c5ce5ddb439d780129
Alejandro Castro <alejandro.castro2016@outlook.es>
2022-05-01 17:40:11 -0500

    Python script to install libs

---
+++ install.py
@@ -0,0 +1,25 @@
+import sys
+import subprocess
+
+# implement pip as a subprocess:
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'psutil'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'zmq'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'websockets'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'python-banyan'])
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'pyserial'])
+
+# process output with an API in the subprocess module:
+reqs = subprocess.check_output([sys.executable, '-m', 'pip',
+'freeze'])
+installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
+
+print(installed_packages)
